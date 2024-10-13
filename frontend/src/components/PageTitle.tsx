1e4a4b0758c8cbcac2471e6995c5512f4e5b0ced
Fi <158217226+lightfi7@users.noreply.github.com>
2024-10-12 17:08:02 -0700

    fixed

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -78,7 +78,11 @@ const PageTitle: React.FC = () => {
     const fetchAudio = async (audioName: string) => {
         if (audioName) {
             var ap = audio;
-            ap.pause();
+            if (ap.currentTime != 0 && ap.currentTime < ap.duration && !ap.paused) {
+                ap.pause();
+                setIsPlaying(false);
+                return
+            }
             setProgress(0);
             try {
                 setIsPlaying(true);
@@ -177,15 +181,12 @@ const PageTitle: React.FC = () => {
                             setCurrentAudioId(pageTitle.ID);
                             setAudioName(pageTitle.audio.slice(0, -4));
                             fetchAudio(pageTitle.audio.slice(0, -4))
-
-                            // Update the URL without reloading the page
-                            // window.history.pushState({}, '', `/page/${pagenumber}/${pageTitle.audio}`);
                         }}>
                             <div>
                                 <span className="font-bold text-xl">{pageTitle?.variant}</span>
                                 <p className="text-sm text-gray-600">{pageTitle?.location} | {pageTitle?.cohort}</p>
                             </div>
-                            <button className="whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground px-4 py-2 w-12 h-12 rounded-full bg-[#777] hover:bg-[#eeeeee] focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex items-center justify-center "
+                            <button className="whitespace-nowrap text-sm font-medium ring-offset-background  transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 text-primary-foreground px-4 py-2 w-12 h-12 rounded-full bg-[#777] hover:bg-[#eeeeee] focus:ring-2 focus:ring-offset-2 focus:ring-gray-100 flex items-center justify-center "
                                 aria-label={isPlaying ? 'Pause' : 'Play'}>
                                 {isPlaying && currentAudioId == pageTitle.ID ? (
                                     <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
